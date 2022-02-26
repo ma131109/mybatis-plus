91dcfe91d8a7459a201dc4a0bbe7dc9d2fabda26
ma131109 <99724037+ma131109@users.noreply.github.com>
2022-02-26 20:06:34 +0800

    Update HelloWorld.java

--- src/main/java/com/itcast/hello/HelloWorld.java
+++ src/main/java/com/itcast/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package com.itcast.hello;
 public class HelloWorld {
     
     private String userName;
+    private Integer age;
     
     public static void main(String[] args) {
         System.out.println("Hello World");
