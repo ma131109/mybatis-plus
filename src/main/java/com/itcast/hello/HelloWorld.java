b5acb7aa8d873c08c7e0eab9649d3ea74db1209b
ma131109 <99724037+ma131109@users.noreply.github.com>
2022-02-26 20:02:05 +0800

    Update HelloWorld.java

--- src/main/java/com/itcast/hello/HelloWorld.java
+++ src/main/java/com/itcast/hello/HelloWorld.java
@@ -1,6 +1,9 @@
 package com.itcast.hello;
 
 public class HelloWorld {
+    
+    private String userName;
+    
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
